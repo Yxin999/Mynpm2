68e7f93feabb40845a96575667b02a0ca7c0487d
Yxin999 <87688706+Yxin999@users.noreply.github.com>
2021-07-20 13:56:27 +0800

    Add files via upload

---
+++ Mynpm.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yx.Mynpm
+{
+    public class Mynpm
+    {
+        public const string str = "what?";
+    }
+}
